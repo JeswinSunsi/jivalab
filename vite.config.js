333c8e4cea5b775296fd6546d012bc459caa137e
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-23 09:27:50 +0530

    Add ngrok to vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    allowedHosts: ['localhost', 'eeda-14-139-184-222.ngrok-free.app', "*", "0.0.0.0", "*.ngrok-free.app"],
+    host: true, // This enables listening on all local IPs
+  }
 })
